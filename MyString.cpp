dbd92fa289c69f8c252a1c00eec3cff0e3e76466
w01164264 <seanbigler@mail.weber.edu>
2017-11-21 12:49:34 -0700

    Initial commit

---
+++ MyString.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Hugo Valle on 11/12/17.
+//
+#include <memory>
+#include "MyString.h"
+
---
+++ MyString.h
@@ -0,0 +1,19 @@
+//
+// Created by Hugo Valle on 11/12/17.
+//
+
+#ifndef HW7_MYSTRING_H
+#define HW7_MYSTRING_H
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <memory>
+//#include <string> // THIS IS NOT ALLOWED. No C++ string class allowed
+using namespace std;
+
+class MyString
+{
+
+};
+
+#endif //HW7_MYSTRING_H
